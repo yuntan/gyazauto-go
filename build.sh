db1a0e0f05579a29dbdf18e439cfbfccb1d5ce4b
Yuto Tokunaga <yuntan.sub1@gmail.com>
2017-10-21 22:43:45 +0900

    fix build script

--- build.sh
+++ build.sh
@@ -5,7 +5,7 @@ build () {
   arch=$2
   echo "for ${os} ${arch}bit"
 
-  bin=bin/gyazauto-$(git describe)-$os
+  bin=bin/gyazauto-$(git describe)-$os$arch
   if [[ $os == "windows" ]]; then
     bin=$bin.exe
   fi
@@ -13,18 +13,18 @@ build () {
   if [[ $arch == "32" ]]; then
     arch_="386"
   fi
-  GOOS=$os GOARCH=$arch_ go build -o $bin -ldflags "-X main.version=$(git describe)" github.com/yuntan/gyazauto/cmd/gyazauto
+  GOOS=$os GOARCH=$arch_ go build -o $bin -ldflags "-X main.version=$(git describe)" github.com/yuntan/gyazauto-go/cmd/gyazauto
 }
 
 if [ $# == 2 ]; then
-  build_and_compress $1 $2
+  build $1 $2
   exit 0
 fi
 
 for os in linux darwin windows; do
-  build_and_compress $os 64
+  build $os 64
 done
 
 for os in linux windows; do
-  build_and_compress $os 32
+  build $os 32
 done
